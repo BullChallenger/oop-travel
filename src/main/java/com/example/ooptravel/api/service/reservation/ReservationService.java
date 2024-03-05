8350509b1d3ac91fd9a31583536eddf897de3b73
BullChallenger <am23am45@naver.com>
2024-03-05 21:44:09 +0900

    .

--- src/main/java/com/example/ooptravel/api/service/reservation/ReservationService.java
+++ src/main/java/com/example/ooptravel/api/service/reservation/ReservationService.java
@@ -1,10 +1,6 @@
 package com.example.ooptravel.api.service.reservation;
 
-import com.example.ooptravel.api.service.reservation.request.ReservationOrder;
-import com.example.ooptravel.domain.hotel.Hotel;
 import com.example.ooptravel.domain.hotel.repository.HotelRepository;
-import com.example.ooptravel.domain.reservation.Reservation;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.ooptravel.domain.reservation.repository.ReservationRepository;
@@ -17,9 +13,5 @@ public class ReservationService {
 
 	private final ReservationRepository reservationRepository;
 	private final HotelRepository hotelRepository;
-
-	public void createReservation(ReservationOrder request) {
-
-	}
 
 }
